8746deef1ceb78d28ddede4a796fae85f3b605ff
Athena0070 <fomenkyt@gmail.com>
2023-09-17 04:35:00 -0400

    my first commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo -n "please enter your name: " 
+read name 
+echo -n "please enter your team: " 
+read team 
+echo "OHH Nice, welcome to Etech Devops master class $name and you are also welcome to $team" 
+echo "End of script"
