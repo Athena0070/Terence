551450b524f74fbda10827a2aef2b41ba278bf80
Athena0070 <144500088+Athena0070@users.noreply.github.com>
2023-10-05 00:50:10 -0700

    Update script.sh
    updsted for practice

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ read name
 echo -n "please enter your team: " 
 read team 
 echo "OHH Nice, welcome to Etech Devops master class $name and you are also welcome to $team" 
-echo "End of script"
+echo "End of script" 
+this is for demo
